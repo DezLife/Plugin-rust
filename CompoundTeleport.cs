d97dc67d0830447805c5914a2fb181c92ad67829
DezLife <69988794+DezLife@users.noreply.github.com>
2020-08-23 20:35:57 +0300

    Refactoring

--- CompoundTeleport.cs
+++ CompoundTeleport.cs
@@ -9,12 +9,22 @@ namespace Oxide.Plugins
     [Description("Teleport to npc city")]
     class CompoundTeleport : RustPlugin
     {
-
 		#region Variables
-		MonumentInfo monument;
-		private Dictionary<BasePlayer, SleepingBag> bags = new Dictionary<BasePlayer, SleepingBag>();
+		private Dictionary<string, MonumentInfo> positions = new Dictionary<string, MonumentInfo>();
+		private Dictionary<BasePlayer, SleepingBag[]> bags = new Dictionary<BasePlayer, SleepingBag[]>();
 		private Queue<SleepingBag> bagsPool = new Queue<SleepingBag>();
-		List<Vector3> Positions = new List<Vector3>()
+		List<Vector3> PositionsOutPost = new List<Vector3>()
+        {
+            new Vector3(-6.4f, 0, 3.5f),
+            new Vector3(-12.4f, 0, 17.5f),
+            new Vector3(27.4f, 3, -17.5f),
+            new Vector3(24.4f, 0, 10.5f),
+            new Vector3(23.4f, 0, 15.5f),
+            new Vector3(12.4f, 0, 17.5f),
+            new Vector3(-15.4f, 0, 17.5f),
+            new Vector3(-26.4f, 2.55f, 28.5f)
+        };
+		List<Vector3> PositionsBandit = new List<Vector3>()
 		{
 			new Vector3(-6.4f, 0, 3.5f),
 			new Vector3(-12.4f, 0, 17.5f),
@@ -34,14 +44,24 @@ namespace Oxide.Plugins
 		{
 			[JsonProperty("Sleeping bag Cooldown")]
 			public int cooldown;
-			[JsonProperty("Sleeping bag name")]
-			public string bagName;
+			[JsonProperty("The opportunity respawn in a outpost")]
+			public bool outPostRespawn;
+			[JsonProperty("Name bag outpost")]
+			public string bagNameOutPost;
+			[JsonProperty("The opportunity respawn in a Bandit Town")]
+			public bool banditRespawn;
+			[JsonProperty("Name bag Bandit Town")]
+			public string bagNameBandit;
 			public static Configuration GetNewConfiguration()
 			{
 				return new Configuration
 				{
 					cooldown = 150,
-					bagName = "OUTPOST"
+					outPostRespawn = true,
+					bagNameOutPost = "OUTPOST",
+					banditRespawn = true,
+					bagNameBandit = "BANDIT TOWN"
+					
 				};
 			}
 		}
@@ -66,75 +86,95 @@ namespace Oxide.Plugins
 		protected override void SaveConfig() => Config.WriteObject(config);
 
 
-		#endregion
+        #endregion
 
-		#region OxideHooks
-		private void OnServerInitialized()
+        #region OxideHooks
+        void Unload()
+        {
+            foreach (var player in BasePlayer.activePlayerList)
+                OnPlayerDisconnected(player);
+        }
+
+        object OnPlayerRespawn(BasePlayer p, SleepingBag bag)
+        {
+			if(bags[p].Where(x => x.net.ID == bag.net.ID).Count() > 0)
+            {
+				var pos = bag.niceName == config.bagNameBandit ? PositionsBandit : PositionsOutPost;
+				bag.transform.position = positions[bag.niceName].transform.position + positions[bag.niceName].transform.rotation * pos.GetRandom();
+			}
+            return false;
+        }
+        object OnServerCommand(ConsoleSystem.Arg arg)
+        {
+            uint netId = arg.GetUInt(0, 0);
+            if (arg.cmd.Name.ToLower() == "respawn_sleepingbag_remove" && netId != 0)
+            {
+                BasePlayer basePlayer = arg.Player();
+                if (!basePlayer)
+                    return false;
+
+                if (bags[basePlayer].Where(bag => bag.net.ID == netId).Count() != 0)
+                    return false;
+            }
+            return null;
+        }
+        private void OnServerInitialized()
 		{
-			monument = UnityEngine.Object.FindObjectsOfType<MonumentInfo>().FirstOrDefault(p => p.name.Contains("compound"));
-
+			foreach (MonumentInfo monument in UnityEngine.Object.FindObjectsOfType<MonumentInfo>())
+			{
+				if (monument.name.ToLower().Contains("compound") && config.outPostRespawn)
+                {
+					positions.Add(config.bagNameOutPost, monument);
+				}
+				else if (monument.name.Contains("bandit") && config.banditRespawn)
+                {
+					positions.Add(config.bagNameBandit, monument);
+				}
+			}
 			foreach (var player in BasePlayer.activePlayerList)
 				OnPlayerConnected(player);
 		}
-		object OnPlayerRespawn(BasePlayer p, SleepingBag bag)
-		{
-			if (bag.net.ID == bags[p]?.net.ID)
-			{
-				bag.transform.position = monument.transform.position + monument.transform.rotation * Positions.GetRandom();
-			}
-			return false;
-		}
-		object OnServerCommand(ConsoleSystem.Arg arg)
-		{
-			uint netId = arg.GetUInt(0, 0);
-			if (arg.cmd.Name.ToLower() == "respawn_sleepingbag_remove" && netId != 0)
-			{
-				BasePlayer basePlayer = arg.Player();
-				if (!basePlayer)
-					return false;
-
-				if (bags[basePlayer]?.net?.ID == netId)
-					return false;
-			}
-			return null;
-		}
-		void Unload()
-		{
-			foreach (var player in BasePlayer.activePlayerList)
-				OnPlayerDisconnected(player);
-		}
-
 		private void OnPlayerConnected(BasePlayer d)
 		{
-			SleepingBag bag = FromPool(d);
+			int count = positions.Count, idx = -1;
 
 			if (!bags.ContainsKey(d))
-				bags.Add(d, bag);
+				bags.Add(d, new SleepingBag[count]);
 
-			SleepingBag.sleepingBags.Add(bag);
+			foreach (var positionKvp in positions)
+			{
+				SleepingBag bag = FromPool(d);
+				bag.niceName = positionKvp.Key;
+				bag.transform.position = positionKvp.Value.transform.position;
+
+				bags[d][++idx] = bag;
+
+				SleepingBag.sleepingBags.Add(bag);
+			}
 		}
 
 		private void OnPlayerDisconnected(BasePlayer d)
 		{
-			SleepingBag bag;
-
-			if (!bags.TryGetValue(d, out bag))
+			if (!bags.ContainsKey(d))
 				return;
 
-			SleepingBag.sleepingBags.Remove(bag);
-			ResetToPool(bag);
+			foreach (SleepingBag bag in bags[d])
+			{
+				SleepingBag.sleepingBags.Remove(bag);
+				ResetToPool(bag);
+			}
 		}
-		#endregion
+        #endregion
 
-		#region Helpers
-		private SleepingBag FromPool(BasePlayer d)
+        #region Helpers
+        private SleepingBag FromPool(BasePlayer d)
 		{
 			SleepingBag bag;
 
 			if (bagsPool.Count > 0)
 			{
 				bag = bagsPool.Dequeue();
-				bag.deployerUserID = d.userID;
+				bag.OwnerID = d.userID;
 
 				return bag;
 			}
@@ -145,11 +185,10 @@ namespace Oxide.Plugins
 			bag.deployerUserID = d.userID;
 			bag.net = Network.Net.sv.CreateNetworkable();
 
-			bag.niceName = config.bagName;
-			bag.transform.position = monument.transform.position;
+			bag.secondsBetweenReuses = config.cooldown;
+			bag.transform.position = Vector3.one;
 			bag.RespawnType = ProtoBuf.RespawnInformation.SpawnOptions.RespawnType.Bed;
 			bag.unlockTime = 0;
-			bag.secondsBetweenReuses = config.cooldown;
 
 			return bag;
 		}
@@ -158,7 +197,6 @@ namespace Oxide.Plugins
 		{
 			bagsPool.Enqueue(bag);
 		}
-		#endregion
-
-	}
+        #endregion
+    }
 }
